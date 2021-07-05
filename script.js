819b9431def2facf5850679af1ef6eebdef92700
anecanabarro <anelisespaz@gmail.com>
2021-07-05 20:12:22 -0300

    cria comida

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake [0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "violet"; /*cor da caixa*/
@@ -20,17 +24,38 @@ function criarCobrinha(){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "blue"; /*cor da comida*/
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x; /*array posição 0 de x*/
     let snakeY = snake[0].y; /*array posição 0 de y*/
 
     if(direction == "right") snakeX += box; /*condicional, se a posição de snakeX for right, acrescenta uma caixa*/
     if(direction == "left") snakeX -= box; /*condicional, pra esquerda, diminui uma caixa*/
-    if(direction == "up") snakeY += box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     snake.pop(); /*retira último elemento do array*/
 
@@ -40,7 +65,7 @@ function iniciarJogo(){
     }
 
     snake.unshift(newHead);
-    
+
 }
 
 let jogo = setInterval(iniciarJogo, 100); /*dá continuidade ao jogo*/
